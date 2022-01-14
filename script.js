ee0d53059e7d01a9d54c084706fb39c7344e99cf
Sayed Haamid Tore <toreshcoding@gmail.com>
2022-01-14 16:22:55 -0500

    created main drawing fucntion and began adding buttons

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.getElementById("container");
 const buttons = document.getElementById("buttons");
+const gridToggle = document.getElementById("grid-toggle");
 
 function makeCanvas(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
@@ -7,7 +8,29 @@ function makeCanvas(rows, cols) {
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
-  };
+  }
+  
+  changeCellColor();
 };
 
 makeCanvas(16, 16);
+
+// changes color of each cell to black
+function changeCellColor() {
+  let cellColors = document.querySelectorAll(".grid-item");
+  cellColors.forEach(cellColor => {
+    cellColor.addEventListener('mouseover', () => {
+      cellColor.style.backgroundColor = "black";
+    });
+  });
+}
+
+
+
+
+
+
+
+
+
+
